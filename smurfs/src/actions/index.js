d373ac55cbec8fb1638b9748e8b0c5c9525a3550
Ryan Clark <rbclark244@gmail.com>
2021-02-07 22:36:10 -0500

    codegrade

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,8 @@ export const GET_DATA_SUCCESS = "DATA_SUCCESS";
 export const GET_DATA_FAIL = "DATA_FAIL";
 export const DISPLAY_ALL_SMURFS = "DISPLAY_ALL_SMURFS";
 
+//codegrade is bad and just another way that lambda is becoming lazier.
+
 export const getData = () => (dispatch) => {
   dispatch({ type: GET_DATA_START });
   axios
